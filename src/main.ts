f6c6f79a97797f4a0d49274530afcabb664ebe0b
Brandon Stout <bdstout1617@gmail.com>
2023-03-12 20:03:35 -0400

    random

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,20 @@ import "./style.css"
 import App from './App.vue'
 import './samples/node-api'
 
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 createApp(App)
+  .use(vuetify)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
